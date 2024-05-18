531fab411ec86d68d1b6eebf44e9594664a21426
Dikshant23529 <latherdikshant0@gmail.com>
2024-05-18 19:30:07 +0530

    circular Linked list print

---
+++ CircularSinglyLinkedList.java
@@ -0,0 +1,83 @@
+public class CircularSinglyLinkedList {
+
+    private ListNode last;
+
+    private int length;
+
+    private class ListNode {
+        private ListNode next;
+        private int data;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public CircularSinglyLinkedList() {
+        last = null;
+        length = 0;
+    }
+
+    public boolean isEmpty() {
+        if (length == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public void createCircualLinkedList() {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode forth = new ListNode(4);
+
+        first.next = second;
+        second.next = third;
+        third.next = forth;
+        forth.next = first;
+
+        last = forth;
+    }
+
+    /*
+     * public void printList() {
+     * ListNode temp = last;
+     * while (temp != null) {
+     * System.out.print(temp.data + "-->");
+     * temp = temp.next;
+     * if (temp.data == 4) {
+     * System.out.println();
+     * System.out.println("breaked");
+     * break;
+     * }
+     * }
+     * }
+     */
+
+    public void printList() {
+        if (last == null) {
+            return;
+        }
+
+        ListNode first = last.next;
+
+        while (first != last) {
+            System.out.print(first.data + "-->");
+            first = first.next;
+        }
+
+        System.out.print(first.data + "");
+    }
+
+    public static void main(String[] args) {
+        CircularSinglyLinkedList doublyLinkedList = new CircularSinglyLinkedList();
+        doublyLinkedList.createCircualLinkedList();
+        doublyLinkedList.printList();
+    }
+
+}
