b32b7b963acfceab1bed025962bc1afa8038e8fa
Dikshant23529 <latherdikshant0@gmail.com>
2024-05-19 12:09:53 +0530

    stack with linked list

--- StackInJava.java
+++ StackInJava.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 public class StackInJava {
 
     private ListNode top;
@@ -12,7 +14,6 @@ public class StackInJava {
 
         public ListNode(int data) {
             this.data = data;
-            this.next = null;
         }
 
     }
@@ -22,6 +23,54 @@ public class StackInJava {
         temp.next = top;
         top = temp;
         length++;
+    }
+
+    public int pop() {
+
+        if (length == 0) {
+            throw new EmptyStackException();
+        }
+
+        int result = top.data;
+        top = top.next;
+        length--;
+        return result;
+    }
+
+    public StackInJava() {
+        top = null;
+        length = 0;
+    }
+
+    public boolean isEmpty() {
+        if (length == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return top.data;
+    }
+
+    public static void main(String[] args) {
+        StackInJava stackInJava = new StackInJava();
+        stackInJava.push(10);
+        stackInJava.push(20);
+        stackInJava.push(30);
+        stackInJava.push(40);
+
+        System.out.println(stackInJava.peek());
+
+        stackInJava.pop();
+
+        System.out.println(stackInJava.peek());
+
     }
 
 }
